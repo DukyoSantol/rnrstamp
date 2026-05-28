444a0e67478b06c777680998e2d0b4789e9a6c6d
DukyoSantol <dukyosantol@gmail.com>
2026-05-28 09:34:14 +0800

    Fix: route all traffic through serverless function with includeFiles

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const publicDir = path.join(appDir, 'public');
 
 app.use(cors());
 app.use(express.json());
-if (!process.env.VERCEL) app.use(express.static(publicDir));
+app.use(express.static(publicDir));
 
 // In-memory file map
 const uploadedFiles = new Map();
