5d8dc5a7e90f3de1c7480c339ac552eb837215d7
DukyoSantol <dukyosantol@gmail.com>
2026-05-28 12:50:09 +0800

    Remove vercel.json, use Vercel auto-detection

--- api/index.js
+++ api/index.js
@@ -1,16 +1,3 @@
-const express = require('express');
-const cors = require('cors');
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/health', (req, res) => {
-    res.json({ status: 'ok', receivers: ['Ellen Mancera', 'Shiely Dilangalen'] });
-});
-
-app.get('/api/receivers', (req, res) => {
-    res.json({ receivers: ['Ellen Mancera', 'Shiely Dilangalen'] });
-});
-
-module.exports = app;
+module.exports = (req, res) => {
+  res.json({ status: 'ok', receivers: ['Ellen Mancera', 'Shiely Dilangalen'] });
+};
